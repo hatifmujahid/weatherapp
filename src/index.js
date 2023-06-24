addfdcc7610573f44e1ce062f480c7e2151e1f3e
hatifmujahid <hatifmujahid08@gmail.com>
2023-06-24 17:14:11 +0500

    Revert "localhost implementation working. onto deployment"
    This reverts commit 28e3f2ac58a08546ad9f42915f544ac184640d14.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/api",  //server address
+  cache: new InMemoryCache(),//necessary to have 
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
